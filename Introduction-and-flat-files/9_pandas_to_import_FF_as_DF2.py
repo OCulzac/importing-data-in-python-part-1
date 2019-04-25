9ce41823e9e54e8db9c9b810cd3be12dfc6b288e
oronc <oronculzac@gmail.com>
2019-04-25 15:15:28 -0400

    Adds Using pandas to import flat files as DF 2 exercise file

---
+++ Introduction-and-flat-files/9_pandas_to_import_FF_as_DF2.py
@@ -0,0 +1,13 @@
+""" Using pandas to import flat files as DataFrames (2)
+In the last exercise, you were able to import flat files into a pandas DataFrame. As a bonus, it is then straightforward to retrieve the corresponding numpy array using the attribute values. You'll now have a chance to do this using the MNIST dataset, which is available as digits.csv. """
+# Assign the filename: file
+file = 'digits.csv'
+
+# Read the first 5 rows of the file into a DataFrame: data
+data = pd.read_csv(file, nrows=5, header=None)
+
+# Build a numpy array from the DataFrame: data_array
+data_array = data.values
+
+# Print the datatype of data_array to the shell
+print(type(data_array))
