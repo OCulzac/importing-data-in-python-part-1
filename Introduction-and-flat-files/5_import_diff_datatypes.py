3419eda5385f32c63acc07443f88f7e11952ffc7
oronc <oronculzac@gmail.com>
2019-04-25 14:55:06 -0400

    adds Importing different datatypes exercise file

---
+++ Introduction-and-flat-files/5_import_diff_datatypes.py
@@ -0,0 +1,34 @@
+""" Importing different datatypes
+The file seaslug.txt
+
+has a text header, consisting of strings
+is tab-delimited.
+These data consists of percentage of sea slug larvae that had metamorphosed in a given time period. Read more here.
+
+Due to the header, if you tried to import it as-is using np.loadtxt(), Python would throw you a ValueError and tell you that it could not convert string to float. There are two ways to deal with this: firstly, you can set the data type argument dtype equal to str (for string).
+
+Alternatively, you can skip the first row as we have seen before, using the skiprows argument.
+
+ """
+
+
+# Assign filename: file
+file = 'seaslug.txt'
+
+# Import file: data
+data = np.loadtxt(file, delimiter='\t', dtype=str)
+
+# Print the first element of data
+print(data[0])
+
+# Import data as floats and skip the first row: data_float
+data_float = np.loadtxt(file, delimiter='\t', dtype=float, skiprows=1)
+
+# Print the 10th element of data_float
+print(data_float[9])
+
+# Plot a scatterplot of the data
+plt.scatter(data_float[:, 0], data_float[:, 1])
+plt.xlabel('time (min.)')
+plt.ylabel('percentage of larvae')
+plt.show()
