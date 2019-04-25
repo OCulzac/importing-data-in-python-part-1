446f7c616049f2731c97e46e4f9d3fa720b198a1
oronc <oronculzac@gmail.com>
2019-04-25 15:00:01 -0400

    adds Working with mixed datatypes 2 exercise file

---
+++ Introduction-and-flat-files/7_work_with_mixed_datatypes2.py
@@ -0,0 +1,11 @@
+""" Working with mixed datatypes (2)
+You have just used np.genfromtxt() to import data containing mixed datatypes. There is also another function np.recfromcsv() that behaves similarly to np.genfromtxt(), except that its default dtype is None. In this exercise, you'll practice using this to achieve the same result. """
+
+# Assign the filename: file
+file = 'titanic.csv'
+
+# Import file using np.recfromcsv: d
+d = np.recfromcsv(file, delimiter=',', names=True, dtype=None)
+
+# Print out first three entries of d
+print(d[:3])
