7f021ab6391521502e7a5e0f15574a6d3e7232ba
oronc <oronculzac@gmail.com>
2019-04-25 15:19:49 -0400

    Adds Customizing your pandas import exercise file

---
+++ Introduction-and-flat-files/10_custom_pandas_import.py
@@ -0,0 +1,23 @@
+""" Customizing your pandas import
+The pandas package is also great at dealing with many of the issues you will encounter when importing data as a data scientist, such as comments occurring in flat files, empty lines and missing values. Note that missing values are also commonly referred to as NA or NaN. To wrap up this chapter, you're now going to import a slightly corrupted copy of the Titanic dataset titanic_corrupt.txt, which
+
+contains comments after the character '#'
+is tab-delimited. """
+
+# Import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+
+# Assign filename: file
+file = 'titanic_corrupt.txt'
+
+# Import file: data
+data = pd.read_csv(file, sep='\t', comment="#", na_values='Nothing')
+
+# Print the head of the DataFrame
+print(data.head())
+
+# Plot 'Age' variable in a histogram
+pd.DataFrame.hist(data[['Age']])
+plt.xlabel('Age (years)')
+plt.ylabel('count')
+plt.show()
