9c88190406496948e7ec43927235f8332f8c4ffe
oronc <oronculzac@gmail.com>
2019-04-26 17:24:26 -0400

    Adds 1,2 exercise files 1 Creating a database engine 2 What are the tables in the database?

---
+++ 3-Introduction-to-relational-databases/2_what_are_the_db_tables.py
@@ -0,0 +1,16 @@
+""" What are the tables in the database?
+In this exercise, you'll once again create an engine to connect to 'Chinook.sqlite'. Before you can get any data out of the database, however, you'll need to know what tables it contains!
+
+To this end, you'll save the table names to a list using the method table_names() on the engine and then you will print the list. """
+
+# Import necessary module
+from sqlalchemy import create_engine
+
+# Create engine: engine
+engine = create_engine('sqlite:///Chinook.sqlite')
+
+# Save the table names to a list: table_names
+table_names = engine.table_names()
+
+# Print the table names to the shell
+print(table_names)
